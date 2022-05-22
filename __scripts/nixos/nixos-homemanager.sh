3057d0e375988942f6ac1b64b59c89fd92be3215
computercam <6525179+computercam@users.noreply.github.com>
2022-05-22 09:45:04 -0500

    home manager channel for nixos

--- __scripts/nixos/nixos-homemanager.sh
+++ __scripts/nixos/nixos-homemanager.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+version=`nixos-version | cut -d"." -f 1,2`
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-${version}.tar.gz home-manager
 nix-channel --update
