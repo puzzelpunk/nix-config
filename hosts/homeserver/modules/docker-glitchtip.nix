f0f2514dfb3438a25508c051c5bf535c08e58e27
cameron <csanders@protonmail.com>
2024-03-31 01:31:14 -0500

    fix glitchtip

--- hosts/homeserver/modules/docker-glitchtip.nix
+++ hosts/homeserver/modules/docker-glitchtip.nix
@@ -1,92 +1,109 @@
 { config, lib, pkgs, options, ... }:
 let
-  DATABASE_URL = "postgres://postgres:postgres@postgres:5432/postgres";
   SECRET_KEY = "`cat ${config.age.secrets.glitchtip_key.path}`";
-  PORT = "8000";
-  # Example smtp://email:password@smtp_url:port https://glitchtip.com/documentation/install#configuration
-  EMAIL_URL = "consolemail://";
-  GLITCHTIP_DOMAIN = "https://glitchtip.cameron.computer";
-  DEFAULT_FROM_EMAIL = "contact@cameron.computer";
-  # Scale between 1 and 3 to prevent excessive memory usage. Change it or remove to set it to the number of cpu cores.
-  CELERY_WORKER_AUTOSCALE = "1,3";
-  CELERY_WORKER_MAX_TASKS_PER_CHILD = "10000";
 
-  POSTGRES_HOST_AUTH_METHOD = "trust";
 
-  defaultDependsOn = [ "glitchtip_postgres" "glitchtip_redis" ];
+  rootpath = "/Volumes/Server/docker/glitchtip/";
+  username = "glitchtip";
+  guid = 5000;
+
+
+  defaultEnvironment = {
+    DATABASE_URL = "postgres://postgres:postgres@glitchtippostgres:5432/postgres";
+    REDIS_URL = "redis://glitchtipredis:6379/0";
+    PORT = "8000";
+    EMAIL_URL = "consolemail://"; # Example smtp://email:password@smtp_url:port https://glitchtip.com/documentation/install#configuration
+    GLITCHTIP_DOMAIN = "https://glitchtip.cameron.computer";
+    DEFAULT_FROM_EMAIL = "contact@cameron.computer"; # Scale between 1 and 3 to prevent excessive memory usage. Change it or remove to set it to the number of cpu cores.
+    CELERY_WORKER_AUTOSCALE = "1,3";
+    CELERY_WORKER_MAX_TASKS_PER_CHILD = "10000";
+  };
+
+  defaultDependsOn = [ "glitchtippostgres" "glitchtipredis" ];
+
+  dockerPrestart = ''
+    ENV_FILE="${rootpath}.env.secret"
+
+    SECRET_KEY=${SECRET_KEY}
+  
+    echo "SECRET_KEY=$SECRET_KEY" > $ENV_FILE
+
+    chmod 600 $ENV_FILE
+    chown ${username}:${username} $ENV_FILE
+  '';
 in {
   config = {
     age.secrets = {
       glitchtip_key.file = ../../../secrets/glitchtip_key.age;
     };
 
-    systemd.services.docker-glitchtip.preStart = ''
-      ENV_FILE="/Volumes/Server/docker/glitchtip/.env.secret"
+    users.groups."${username}" = {
+      name = "${username}";
+      gid = guid;
+    };
+    users.users."${username}" = {
+      extraGroups = [ "${username}" ];
+      name = "${username}";
+      uid = guid;
+      group = "${username}";
+      isNormalUser = true;
+      home = rootpath;
+    };
 
-      SECRET_KEY=${SECRET_KEY}
-      DATABASE_URL="${DATABASE_URL}"
-      PORT="${PORT}"
-      EMAIL_URL="${EMAIL_URL}"
-      GLITCHTIP_DOMAIN="${GLITCHTIP_DOMAIN}"
-      DEFAULT_FROM_EMAIL="${DEFAULT_FROM_EMAIL}"
-      CELERY_WORKER_AUTOSCALE="${CELERY_WORKER_AUTOSCALE}"
-      CELERY_WORKER_MAX_TASKS_PER_CHILD="${CELERY_WORKER_MAX_TASKS_PER_CHILD}"
-
-      echo "DATABASE_URL=$DATABASE_URL" > $ENV_FILE
-      echo "SECRET_KEY=$SECRET_KEY" >> $ENV_FILE
-      echo "PORT=$PORT" >> $ENV_FILE
-      echo "EMAIL_URL=$EMAIL_URL" >> $ENV_FILE
-      echo "GLITCHTIP_DOMAIN=$GLITCHTIP_DOMAIN" >> $ENV_FILE
-      echo "DEFAULT_FROM_EMAIL=$DEFAULT_FROM_EMAIL" >> $ENV_FILE
-      echo "CELERY_WORKER_AUTOSCALE=$CELERY_WORKER_AUTOSCALE" >> $ENV_FILE
-      echo "CELERY_WORKER_MAX_TASKS_PER_CHILD=$CELERY_WORKER_MAX_TASKS_PER_CHILD" >> $ENV_FILE
-
-      chmod 600 $ENV_FILE
-      chown root.root $ENV_FILE
-    '';
+    systemd.services.docker-glitchtip.preStart = dockerPrestart;
+    systemd.services.docker-glitchtipworker.preStart = dockerPrestart;
+    systemd.services.docker-glitchtipmigrate.preStart = dockerPrestart;
 
     virtualisation.oci-containers.containers = {
-      glitchtip_postgres = {
+      glitchtippostgres = {
         image = "postgres:15";
         volumes = [
-          "/Volumes/Server/docker/glitchtip/postgres:/var/lib/postgresql/data"
+          "${rootpath}postgres:/var/lib/postgresql/data"
         ];
         environment = {
-          POSTGRES_HOST_AUTH_METHOD = "${POSTGRES_HOST_AUTH_METHOD}";
+          POSTGRES_HOST_AUTH_METHOD = "trust";
         };
         extraOptions =
           [ "--network=${config.cfg.docker.networking.dockernet}" ];
       };
-      glitchtip_redis = {
+      glitchtipredis = {
         image = "redis";
         extraOptions =
           [ "--network=${config.cfg.docker.networking.dockernet}" ];
       };
-      glitchtip_web = {
+      glitchtip = {
         image = "glitchtip/glitchtip";
-        volumes = [ "/Volumes/Server/docker/glitchtip/uploads:/code/uploads" ];
-        ports = [ "${PORT}:8000" ];
-        environmentFiles = [ "/Volumes/Server/docker/glitchtip/.env.secret" ];
+        volumes = [ "${rootpath}uploads:/code/uploads" ];
+        ports = [ "${defaultEnvironment.PORT}:8000" ];
+        environment = defaultEnvironment;
+        environmentFiles = [ "${rootpath}.env.secret" ];
         dependsOn = defaultDependsOn;
         extraOptions =
           [ "--network=${config.cfg.docker.networking.dockernet}" ];
       };
-      glitchtip_worker = {
+      glitchtipworker = {
         image = "glitchtip/glitchtip";
-        entrypoint = "./bin/run-celery-with-beat.sh";
-        environmentFiles = [ "/Volumes/Server/docker/glitchtip/.env.secret" ];
+        environment = defaultEnvironment;
+        environmentFiles = [ "${rootpath}.env.secret" ];
         dependsOn = defaultDependsOn;
-        volumes = [ "/Volumes/Server/docker/glitchtip/uploads:/code/uploads" ];
+        volumes = [ "${rootpath}uploads:/code/uploads" ];
         extraOptions =
           [ "--network=${config.cfg.docker.networking.dockernet}" ];
+        entrypoint = "/bin/bash";
+        cmd = [ "/code/bin/run-celery-with-beat.sh" ];
       };
-      glitchtip_migrate = {
+      glitchtipmigrate = {
         image = "glitchtip/glitchtip";
-        entrypoint = "./manage.py migrate";
         dependsOn = defaultDependsOn;
-        environmentFiles = [ "/Volumes/Server/docker/glitchtip/.env.secret" ];
+        environment = defaultEnvironment;
+        environmentFiles = [ "${rootpath}.env.secret" ];
         extraOptions =
           [ "--network=${config.cfg.docker.networking.dockernet}" ];
+        entrypoint = "/usr/local/bin/python";
+        cmd = [ 
+          "/code/manage.py" 
+          "migrate" 
+        ];
       };
     };
   };
