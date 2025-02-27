48975053dfb827eb1be2bb57acb229293ab0626a
cameron <csanders@protonmail.com>
2025-02-27 16:24:02 -0600

    chore: cleanup module

--- hosts/opus/modules/sanderszone/resiliosync.nix
+++ hosts/opus/modules/sanderszone/resiliosync.nix
@@ -18,32 +18,4 @@ in {
   users.users.rslsync.extraGroups = [ "sharedfiles" ];
 
   networking.firewall.allowedTCPPorts = [ resilio_web_port ];
-  # virtualisation.oci-containers.containers = {
-  #   resilio-sync = {
-  #     image = "linuxserver/resilio-sync";
-  #     ports = [ 
-  #       "${config.cfg.networking.static.ip_address}:8888:8888" 
-  #       "${config.cfg.networking.static.ip_address}:55555:55555" 
-  #     ];
-  #     volumes = [
-  #       "/Volumes/Server/docker/resiliosync/config:/config"
-  #       "/Volumes/Server/docker/resiliosync/downloads:/downloads"
-  #       "/Volumes/Storage:/sync"
-  #     ];
-  #     environment = {
-  #       PUID = "1000";
-  #       PGID = "996";
-  #       TZ = "America/Chicago";
-  #     };
-  #     extraOptions = [ 
-  #       "--network=${config.cfg.docker.networking.dockernet}" 
-  #       "--label=swag=enable" 
-  #     ];
-  #   };
-  # };
-
-  # networking.firewall.allowedTCPPorts = [
-  #   8888
-  #   55555
-  # ];
 }
