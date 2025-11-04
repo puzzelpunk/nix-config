47784440488a9f7938976ad816b1e1177b60d56e
computercam <6525179+computercam@users.noreply.github.com>
2025-11-03 23:11:12 -0600

    move docker net creation to service module as a function

---
+++ modules/nixos/service-docker/dockernet-create.nix
@@ -0,0 +1,28 @@
+{ config, lib, pkgs, dockernetConfig, ... }:
+
+let
+    networkName = dockernetConfig.networkName;
+in
+{
+    systemd.services.dockernet-create = {
+      description = "Create the network bridge dockernet container to container networking.";
+      after = [ "network.target" "docker.service" ];
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig.Type = "oneshot";
+      script =
+        let dockercli = "${config.virtualisation.docker.package}/bin/docker";
+        in ''
+          # Put a true at the end to prevent getting non-zero return code, which will
+          # crash the whole service.
+
+          check=$(${dockercli} network ls | grep "${networkName}" || true)
+          
+          if [ -z "$check" ]; then
+            ${dockercli} network create ${networkName}
+          else
+            echo "${networkName} already exists in docker"
+          fi
+        '';
+    };
+}
--- modules/nixos/service-docker/service-docker.nix
+++ modules/nixos/service-docker/service-docker.nix
@@ -1,7 +1,17 @@
 { config, lib, pkgs, options, ... }:
 with pkgs.stdenv;
 with lib; {
-  imports = [ ./options.nix ];
+  imports = [ 
+    ./options.nix
+    (import ./dockernet-create.nix {
+      config = config;
+      lib = lib;
+      pkgs = pkgs;
+      dockernetConfig = {
+        networkName = config.cfg.docker.networking.dockernet;
+      };
+    })
+  ];
 
   config = {
     users.groups.docker = { };
@@ -16,28 +26,6 @@ with lib; {
       storageDriver = "overlay2";
       extraOptions = ''
         --bip="${config.cfg.docker.networking.bip}" --data-root="${config.cfg.docker.storage_root}" --dns="${config.cfg.docker.networking.dns.primary}" --dns="${config.cfg.docker.networking.dns.secondary}" --iptables=${config.cfg.docker.networking.iptables}'';
-    };
-
-    systemd.services.dockernet-create = {
-      description = "Create the network bridge dockernet container to container networking.";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-
-      serviceConfig.Type = "oneshot";
-      script =
-        let dockercli = "${config.virtualisation.docker.package}/bin/docker";
-        in ''
-          # Put a true at the end to prevent getting non-zero return code, which will
-          # crash the whole service.
-
-          check=$(${dockercli} network ls | grep "${config.cfg.docker.networking.dockernet}" || true)
-          
-          if [ -z "$check" ]; then
-            ${dockercli} network create ${config.cfg.docker.networking.dockernet}
-          else
-            echo "${config.cfg.docker.networking.dockernet} already exists in docker"
-          fi
-        '';
     };
   };
 }
