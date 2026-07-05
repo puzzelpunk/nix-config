6918d113fe639e87ca9f3e0288b839d327601ace
computercam <6525179+computercam@users.noreply.github.com>
2026-07-05 16:40:00 -0500

    Fix typos and remove unused import
    Rename `hostPlayform` to `hostPlatform` and remove unused
    `options` from the function arguments.

--- modules/global/macos.nix
+++ modules/global/macos.nix
@@ -2,11 +2,10 @@
   config,
   lib,
   pkgs,
-  options,
   ...
 }:
 {
   cfg.os.name = "macos";
-  stdenv.hostPlayform.system.stateVersion = 4;
-  stdenv.hostPlayform.system.primaryUser = config.cfg.user.name;
+  stdenv.hostPlatform.system.stateVersion = 4;
+  stdenv.hostPlatform.system.primaryUser = config.cfg.user.name;
 }
